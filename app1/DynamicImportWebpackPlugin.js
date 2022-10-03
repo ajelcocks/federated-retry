411963b40e244231b3d29b2bb01868336a0eb720
Anthony Elcocks <Anthony_Elcocks@nuance.com>
2022-10-03 15:08:04 -0400

    iterate remotes from plugin options

--- app1/DynamicImportWebpackPlugin.js
+++ app1/DynamicImportWebpackPlugin.js
@@ -7,35 +7,39 @@ class DynamicImportWebpackPlugin {
   }
 
   // This gets translated to a string in the tap('Require' method inside the apply method (below)
-  __federated_require = (moduleId, cache, appName) => {
+  __federated_require = (moduleId, cache, remotes) => {
     const WEBPACK_CONTAINER = 'webpack/container/';
-    const referenceId = `${WEBPACK_CONTAINER}reference/${appName}`;
-    const remoteId = `${WEBPACK_CONTAINER}remote/${appName}`;
-  
-    if(moduleId.startsWith(remoteId)) {
-      Object.keys(cache).forEach((key) => {
-        if(key.match(`^${referenceId}|^${remoteId}`)) {
-          const cachedModule = cache[key];
-          if (cachedModule !== undefined && !cachedModule.loaded) {
-            delete cache[referenceId];
-            delete cache[moduleId];
+    remotes.forEach((appName) => {
+      const referenceId = `${WEBPACK_CONTAINER}reference/${appName}`;
+      const remoteId = `${WEBPACK_CONTAINER}remote/${appName}`;
+    
+      if(moduleId.startsWith(remoteId)) {
+        Object.keys(cache).forEach((key) => {
+          if(key.match(`^${referenceId}|^${remoteId}`)) {
+            const cachedModule = cache[key];
+            if (cachedModule !== undefined && !cachedModule.loaded) {
+              delete cache[referenceId];
+              delete cache[moduleId];
+            }
           }
-        }
-      });
-    }
+        });
+      }
+    })
   }
 
   apply(compiler) {
     compiler.hooks.compilation.tap('DynamicImportReload', (compilation) => {
+      // This gets inserted above the webpack function that detects the cached modules
       compilation.mainTemplate.hooks.localVars.tap('Require', (source) => {
         return `
           var ${FEDERATED_RELOAD} = ${this.__federated_require}
           `;
       });
+      // This gets inserted inside of and before code in the webpack function that processes dynamic imports
       compilation.mainTemplate.hooks.require.tap("Resolve", (source) => {
         return `
           const remotes = ['${this._remotes.join("','")}'];
-          ${FEDERATED_RELOAD}(moduleId, __webpack_module_cache__, remotes[0]);
+          ${FEDERATED_RELOAD}(moduleId, __webpack_module_cache__, remotes);
           ${source}
         `;
       });
